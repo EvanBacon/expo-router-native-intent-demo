4858e3ccb1673db70cd974a3c130b3b15b67e84d
evanbacon <bacon@expo.io>
2024-12-30 19:19:19 -0600

    fixed with encodeURIComponent

--- app/+native-intent.ts
+++ app/+native-intent.ts
@@ -8,8 +8,9 @@ export async function redirectSystemPath({
   console.log("redirectSystemPath", path, initial);
 
   if (path.startsWith("file://")) {
+    console.log("remapping file:// to query param");
     // Remap `file://` to be a query param and send to the route which can handle it.
-    return "/modal?file=" + path;
+    return "/modal?file=" + encodeURIComponent(path);
   }
 
   return path;
